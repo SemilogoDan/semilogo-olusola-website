b3aaa7884f6704b3d7c7a2b6b606f3f2f83e17b1
Semilogo(Dan) <52991578+SemilogoDan@users.noreply.github.com>
2025-02-16 15:04:56 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div>
-            <h3 className="text-lg font-semibold mb-4 text-black">Portfolio</h3>
+            <h3 className="text-lg font-semibold mb-4 text-black">Semilogo</h3>
             <div className="flex space-x-4">
               <a
                 href="https://github.com/SemilogoDan"
